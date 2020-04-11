21a7a5ba2f3df7b5344b6bad7318b8f3d6b0dacb
moringa <costamay>
2020-04-11 22:54:23 +0300

    Add changes

--- src/estimator.py
+++ src/estimator.py
@@ -1,21 +1,39 @@
 def estimator(data):
       currentlyInfectedImpact = int(data['reportedCases'] * 10)
       currentlyInfectedSevereImpact = int(data['reportedCases'] * 50)
+      
       days = timeEstimate(data)
       
-      return {
+      infectionRequestTimeImpact = currentlyInfectedImpact * (2 ** (int(days / 3)))
+      infectionRequestTimeSevereImpact = currentlyInfectedSevereImpact * (2 ** (int(days / 3)))
+      
+      severeCaseRequestTimeImpact = int(0.15 * infectionRequestTimeImpact)
+      severeCaseRequestTimeSevereImpact = int(0.15 * infectionRequestTimeSevereImpact)
+      
+      beds = int(0.35 * data['totalHospitalBeds'])
+      
+      bedsRequesteTimeImpact = beds - severeCaseRequestTimeImpact
+      bedsRequesteTimeSevereImpact = beds - severeCaseRequestTimeSevereImpact
+      
+      output = {
         "data": data,
         "impact": {
               "currentlyInfected": currentlyInfectedImpact,
-              "infectionsByRequestedTime": currentlyInfectedImpact * (2 ** (int(days / 3)))
+              "infectionsByRequestedTime": infectionRequestTimeImpact,
+              "severeCasesByRequestedTime": severeCaseRequestTimeImpact,
+              "hospitalBedsByRequestedTime": bedsRequesteTimeImpact
         },
         "severeImpact": {
               "currentlyInfected":currentlyInfectedSevereImpact,
-              "infectionsByRequestedTime": currentlyInfectedSevereImpact * (2 ** (int(days / 3)))
-            
+              "infectionsByRequestedTime": infectionRequestTimeSevereImpact,
+              "severeCasesByRequestedTime": severeCaseRequestTimeSevereImpact,
+              "hospitalBedsByRequestedTime": bedsRequesteTimeSevereImpact
+             
         }
         
       }
+      
+      return output
 
 def timeEstimate(data):
       if data['periodType'] == 'days':
