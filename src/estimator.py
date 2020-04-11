91d956b70e07ea02294c616471a28cc2c4fb091e
moringa <costamay>
2020-04-11 18:34:29 +0300

    Add changes

--- src/estimator.py
+++ src/estimator.py
@@ -1,5 +1,22 @@
 def estimator(data):
-  return data
+      currentlyInfectedImpact = int(data['reportedCases'] * 10)
+      currentlyInfectedSevereImpact = int(data['reportedCases'] * 50)
+      days = timeEstimate(data)
+      
+  return {
+    "data": data,
+    "estimate": {
+      "impact": {
+        "currentlyInfected": currentlyInfectedImpact,
+        "infectionsByRequestedTime": currentlyInfectedImpact * (2 ** (days // 3 )
+      },
+      "severeImpact": {
+        "currentlyInfected":currentlyInfectedSevereImpact,
+        "infectionsByRequestedTime": currentlyInfectedSevereImpact * (2 ** (days // 3)
+        
+      }
+    }
+  }
 
 def timeEstimate(data):
       if data['periodType'] == 'days':
