93b79f650ae6f0552ab6cd03cfca226f534de8f2
moringa <costamay>
2020-04-11 18:19:26 +0300

    Add changes

--- src/estimator.py
+++ src/estimator.py
@@ -1,2 +1,11 @@
 def estimator(data):
   return data
+
+def timeEstimate(data):
+      if data['periodType'] == 'days':
+            days = data['timeToElapse']
+      elif data['periodType'] == 'weeks':
+            days = data['timeToElapse'] * 7
+      elif data['periodType'] == 'months':
+            days = data['timeToElapse'] * 30
+      return days
